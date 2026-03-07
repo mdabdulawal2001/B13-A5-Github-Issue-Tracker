935b01d2bc473d9974b678e37314c1943dc8aa7d
toha245 <mdabdulawal2001@gmail.com>
2026-03-07 13:37:14 +0600

    added condition for input fields of form

--- script/script.js
+++ script/script.js
@@ -1,34 +1,27 @@
+document.getElementById("input-form").addEventListener("submit", (event) => {
+  event.preventDefault();
 
+  const inputUser = document.getElementById("input-user").value;
+  const inputPassword = document.getElementById("input-password").value;
 
-document.getElementById("input-form").addEventListener ("submit", (event) => {
-    event.preventDefault();
+  if (!inputUser) {
+    alert("Please Provide a User Name");
+    return;
+  }
 
-    const inputUser = document.getElementById("input-user").value;
-    const inputPassword = document.getElementById("input-password").value;
-    
-    if(inputUser === ""){
-        alert ("Please Provide a User Name");
-        return;
-    }
-    else if(inputPassword === ""){
-        alert("Please Provide the Password");
-        return;
-    }
-    else if(inputUser !== "admin"){
-        alert("Please Provide a valid User Name");
-        return;
-    } 
-    else (inputPassword !== "admin123"){
-        alert("Please Provide a valid Password");
-        return;
-    }
+  if (inputUser !== "admin") {
+    alert("Please Provide a valid User Name");
+    return;
+  }
+  if (!inputPassword) {
+    alert("Please Provide the Password");
+    return;
+  }
+  if (inputPassword !== "admin123") {
+    alert("Please Provide a valid Password");
+    return;
+  }
 
-    // window.location.href = "../main-page.html";
-    window.location.assign("../main-page.html");
-
-    event.target.reset();
-})
-
-
-
-
+  // window.location.href = "../main-page.html";
+  window.location.assign("../main-page.html");
+});
