e1b5a333847b512a0dfb1e5025a81f9a9e3f43a7
toha245 <mdabdulawal2001@gmail.com>
2026-03-07 19:32:47 +0600

    added navbar and dashboard buttons of main page

--- script/main.js
+++ script/main.js
@@ -14,18 +14,18 @@ const getIssues = async () => {
 
 getIssues();
 
-const renderCard = (data) => {
-  data.data.forEach((element) => {
-    console.log(element);
-    const div = document.createElement('div');
-    div.classList.add('border');
-    div.classList.add('m-5');
-    div.innerHTML += `
-        <h1 class="">${element.title}</h1>
-        <p>${element.description}</p>
-        <span>${element.labels[0]}</span>
-        <span>${element.labels[1]}</span>
-    `;
-    cardContainer.appendChild(div)
-  });
-};
+// const renderCard = (data) => {
+//   data.data.forEach((element) => {
+//     console.log(element);
+//     const div = document.createElement('div');
+//     div.classList.add('border');
+//     div.classList.add('m-5');
+//     div.innerHTML += `
+//         <h1 class="">${element.title}</h1>
+//         <p>${element.description}</p>
+//         <span>${element.labels[0]}</span>
+//         <span>${element.labels[1]}</span>
+//     `;
+//     cardContainer.appendChild(div)
+//   });
+// };
